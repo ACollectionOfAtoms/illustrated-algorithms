458912d0a3930c7d3d4b6281af2bb8753ff04910
Adam <davidatomhernandez@gmail.com>
2017-02-14 10:31:20 -0500

    Updating quicksort to handle arrays of duplicate values

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,19 +1,18 @@
-function random(list) {
-  return list[Math.round(Math.random() * (list.length - 1))];
+function randomIndex(list) {
+  return Math.round(Math.random() * (list.length - 1));
 }
 
 export default function quicksort(list) {
   if (list.length < 2) {
     return list;
   }
-
-  const pivot = random(list);
-  const less = list.filter(i => i < pivot);
+  const pivot = list.splice(randomIndex(list), 1);
+  const less = list.filter(i => i <= pivot);
   const greater = list.filter(i => i > pivot);
 
   return [
     ...quicksort(less),
-    pivot,
+    ...pivot,
     ...quicksort(greater)
   ];
 }
